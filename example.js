ebe15a0d606b34144e6516b38119d78655177d07
Alexey Novak <alexeynovak@globalive.com>
2016-03-07 16:33:52 -0500

    upgrade: adding promise support. Refactoring the code

--- example.js
+++ example.js
@@ -3,10 +3,12 @@
 var /* googleStocks = require('google-stocks'), // Use this require if you load module through npm */
     googleStocks = require('./dist/google-stocks.js');
 
-googleStocks.get(['AAPL'], function(error, data) {
+console.log(googleStocks);
+
+googleStocks(['AAPL'], function(error, data) {
   console.log(data);
 });
 
-googleStocks.get(['TSE:WJA', 'NASDAQ:GOOG', 'AAPL'], function(error, data) {
+googleStocks(['TSE:WJA', 'NASDAQ:GOOG', 'AAPL'], function(error, data) {
   console.log(data);
 });
--- test-dist/google-stocks-test.js
+++ test-dist/google-stocks-test.js
@@ -10,69 +10,138 @@ var _distGoogleStocksJs2 = _interopRequireDefault(_distGoogleStocksJs);
 
 describe('google-stocks tests', function () {
 
-  it('nothing was passed', function () {
-    _distGoogleStocksJs2['default'].get(undefined, function (error, data) {
-      (0, _chai.expect)(error).to.equal('No Stocks were passed');
+  describe('callback based', function () {
+    it('nothing was passed', function () {
+      (0, _distGoogleStocksJs2['default'])(undefined, function (error) {
+        (0, _chai.expect)(error).to.equal('No Stocks were passed');
+      });
+    });
+
+    it('empty array', function () {
+      (0, _distGoogleStocksJs2['default'])([], function (error) {
+        (0, _chai.expect)(error).to.equal('No Stocks were passed');
+      });
+    });
+
+    it('with a non existing code', function () {
+      var codes = ['ZXY11_'];
+
+      (0, _distGoogleStocksJs2['default'])(codes, function (error, data) {
+        (0, _chai.expect)(error).to.equal('Webservice returned 400');
+        (0, _chai.expect)(data).to.be.undefined;
+      });
+    });
+
+    it('proper stock code', function (done) {
+      var codes = ['AAPL'];
+
+      (0, _distGoogleStocksJs2['default'])(codes, function (error, data) {
+        (0, _chai.expect)(error).to.be['null'];
+        (0, _chai.expect)(data.length).to.equal(1);
+        (0, _chai.expect)(data.shift().t).to.equal(codes.shift());
+        done();
+      });
+    });
+
+    it('with a different market code', function (done) {
+      var market = 'TSE';
+      var code = 'WJA';
+      var codes = [market + ':' + code];
+
+      (0, _distGoogleStocksJs2['default'])(codes, function (error, data) {
+        (0, _chai.expect)(error).to.be['null'];
+        (0, _chai.expect)(data.length).to.equal(1);
+
+        var result = data.shift();
+        (0, _chai.expect)(result.t).to.equal(code);
+        (0, _chai.expect)(result.e).to.equal(market);
+        done();
+      });
+    });
+
+    it('with a an existing code and non existing code', function (done) {
+      var codes = ['ZXY11_', 'GOOG'];
+
+      (0, _distGoogleStocksJs2['default'])(codes, function (error, data) {
+        (0, _chai.expect)(data.length).to.equal(1);
+        done();
+      });
+    });
+
+    it('with both existing codes and different markets', function (done) {
+      var codes = ['TSE:WJA', 'GOOG'];
+
+      (0, _distGoogleStocksJs2['default'])(codes, function (error, data) {
+        (0, _chai.expect)(data.length).to.equal(2);
+        done();
+      });
     });
   });
 
-  it('empty array', function () {
-    _distGoogleStocksJs2['default'].get([], function (error, data) {
-      (0, _chai.expect)(error).to.equal('No Stocks were passed');
+  describe('promise based', function () {
+    it('nothing was passed', function () {
+      (0, _distGoogleStocksJs2['default'])(undefined)['catch'](function (error) {
+        return (0, _chai.expect)(error).to.equal('No Stocks were passed');
+      });
     });
-  });
 
-  it('with a non existing code', function () {
-    var codes = ['ZXY11_'];
-
-    _distGoogleStocksJs2['default'].get(codes, function (error, data) {
-      (0, _chai.expect)(error).to.equal('Webservice returned 400');
-      (0, _chai.expect)(data).to.be.undefined;
+    it('empty array', function () {
+      (0, _distGoogleStocksJs2['default'])(undefined)['catch'](function (error) {
+        return (0, _chai.expect)(error).to.equal('No Stocks were passed');
+      });
     });
-  });
 
-  it('proper stock code', function (done) {
-    var codes = ['AAPL'];
+    it('with a non existing code', function () {
+      var codes = ['ZXY11_'];
 
-    _distGoogleStocksJs2['default'].get(codes, function (error, data) {
-      (0, _chai.expect)(error).to.be['null'];
-      (0, _chai.expect)(data.length).to.equal(1);
-      (0, _chai.expect)(data.shift().t).to.equal(codes.shift());
-      done();
+      (0, _distGoogleStocksJs2['default'])(codes).then(function (data) {
+        return (0, _chai.expect)(data).to.be.undefined;
+      })['catch'](function (error) {
+        return (0, _chai.expect)(error).to.equal('Webservice returned 400');
+      });
     });
-  });
 
-  it('with a different market code', function (done) {
-    var market = 'TSE',
-        code = 'WJA',
-        codes = [market + ':' + code];
+    it('proper stock code', function (done) {
+      var codes = ['AAPL'];
 
-    _distGoogleStocksJs2['default'].get(codes, function (error, data) {
-      (0, _chai.expect)(error).to.be['null'];
-      (0, _chai.expect)(data.length).to.equal(1);
-
-      var result = data.shift();
-      (0, _chai.expect)(result.t).to.equal(code);
-      (0, _chai.expect)(result.e).to.equal(market);
-      done();
+      (0, _distGoogleStocksJs2['default'])(codes).then(function (data) {
+        (0, _chai.expect)(data.length).to.equal(1);
+        (0, _chai.expect)(data.shift().t).to.equal(codes.shift());
+        done();
+      });
     });
-  });
 
-  it('with a an existing code and non existing code', function (done) {
-    var codes = ['ZXY11_', 'GOOG'];
+    it('with a different market code', function (done) {
+      var market = 'TSE';
+      var code = 'WJA';
+      var codes = [market + ':' + code];
 
-    _distGoogleStocksJs2['default'].get(codes, function (error, data) {
-      (0, _chai.expect)(data.length).to.equal(1);
-      done();
+      (0, _distGoogleStocksJs2['default'])(codes).then(function (data) {
+        (0, _chai.expect)(data.length).to.equal(1);
+
+        var result = data.shift();
+        (0, _chai.expect)(result.t).to.equal(code);
+        (0, _chai.expect)(result.e).to.equal(market);
+        done();
+      });
     });
-  });
 
-  it('with both existing codes and different markets', function (done) {
-    var codes = ['TSE:WJA', 'GOOG'];
+    it('with a an existing code and non existing code', function (done) {
+      var codes = ['ZXY11_', 'GOOG'];
 
-    _distGoogleStocksJs2['default'].get(codes, function (error, data) {
-      (0, _chai.expect)(data.length).to.equal(2);
-      done();
+      (0, _distGoogleStocksJs2['default'])(codes).then(function (data) {
+        (0, _chai.expect)(data.length).to.equal(1);
+        done();
+      });
+    });
+
+    it('with both existing codes and different markets', function (done) {
+      var codes = ['TSE:WJA', 'GOOG'];
+
+      (0, _distGoogleStocksJs2['default'])(codes).then(function (data) {
+        (0, _chai.expect)(data.length).to.equal(2);
+        done();
+      });
     });
   });
 });
